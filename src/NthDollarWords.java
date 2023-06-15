be4fa52a4e6c9d9da136aedf785ff9e8eafbc8a0
BuildTools <unconfigured@null.spigotmc.org>
2023-06-15 16:05:41 -0400

    modified name

--- src/NthDollarWords.java
+++ src/NthDollarWords.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class NthDollarWordsDriver {
+public class NthDollarWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("What file would you like to source words from? ");
